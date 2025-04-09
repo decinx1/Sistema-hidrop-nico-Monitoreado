c2968d62e940b2c0eaf93c91e9c1ff14e4a449ec
vjLarios <vjosuee05@gmail.com>
2025-04-09 13:28:30 -0600

    Vista home responsive, botones con logica para que solo uno este check

--- Interfaz/home.py
+++ Interfaz/home.py
@@ -1,11 +1,20 @@
 import sys
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QButtonGroup
+from PyQt6.QtGui import QIcon  # Importa QIcon para manejar íconos
 from PyQt6.uic import loadUi
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = loadUi("home.ui")  # carga la interfaz de usuario desde el archivo .ui
+    button_group = QButtonGroup(window)
+    # Establecemos que el grupo sea exclusivo: sólo un botón puede estar checkeado a la vez
+    button_group.setExclusive(True)
+    
+    # Agregamos los botones al grupo
+    button_group.addButton(window.btnHome)
+    button_group.addButton(window.btnDatos)
+    button_group.addButton(window.btnHistorial)
     window.setWindowTitle("HydroTech")  # establece el título de la ventana
-    #window.setWindowIcon(QIcon("icono.ico"))  # Establece el icono de la ventana
+    #window.setWindowIcon(QIcon("Iconos/icono.ico"))  # Establece el icono de la ventana
     window.showMaximized()
     sys.exit(app.exec())
