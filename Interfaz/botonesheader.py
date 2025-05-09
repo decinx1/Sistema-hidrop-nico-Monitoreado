131413f1f5288b327ce5e1e3cdf433ef1083186c
vjLarios <vjosuee05@gmail.com>
2025-05-09 17:02:45 -0600

    archivo dedicado para botones, iconos agregados en botones

---
+++ Interfaz/botonesheader.py
@@ -0,0 +1,28 @@
+from PyQt6.QtWidgets import QWidget, QButtonGroup, QStackedWidget
+from PyQt6.uic import loadUi
+from PyQt6.QtGui import QIcon
+
+class BotonesHeader(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # Cargar el diseño desde el archivo UI
+        loadUi("Interfaz/botonesHead.ui", self)
+
+        # Grupo de botones
+        self.button_group = QButtonGroup(self)
+        self.button_group.addButton(self.findChild(QWidget, "btnHome"))
+        self.button_group.addButton(self.findChild(QWidget, "btnDatos"))
+        self.button_group.addButton(self.findChild(QWidget, "btnHistorial"))
+
+        # Encuentra botones específicos
+        self.btn_home = self.findChild(QWidget, "btnHome")
+        self.btn_datos = self.findChild(QWidget, "btnDatos")
+        self.btn_historial = self.findChild(QWidget, "btnHistorial")
+
+        self.btn_home.setChecked(True)
+
+        # Asignar iconos a los botones
+        self.btn_home.setIcon(QIcon("Interfaz/icons/grafica.png"))
+        self.btn_datos.setIcon(QIcon("Interfaz/icons/info.png"))
+        self.btn_historial.setIcon(QIcon("Interfaz/icons/historial.png"))
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QWidget, QVBoxLayout,
-    QHBoxLayout, QButtonGroup, QStackedWidget, QLabel
+    QHBoxLayout, QStackedWidget, QLabel
 )
 from PyQt6.uic import loadUi
 from PyQt6.QtCore import Qt
@@ -11,6 +11,7 @@ from PyQt6.QtCore import Qt
 from Interfaz.home import HomeWindow 
 from Interfaz.datos import DatosView 
 from Interfaz.sidebar import Sidebar
+from Interfaz.botonesheader import BotonesHeader
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -37,40 +38,25 @@ class MainWindow(QMainWindow):
         main_layout.addWidget(sidebar_widget)
         
         # Parte superior: botones
-        botones_widget = QWidget()
-        loadUi("Interfaz/botonesHead.ui", botones_widget)
+        botones_widget = BotonesHeader()
         content_layout.addWidget(botones_widget)
-
-        # Grupo de botones
-        self.button_group = QButtonGroup(self)
-        self.button_group.addButton(botones_widget.findChild(QWidget, "btnHome"))
-        self.button_group.addButton(botones_widget.findChild(QWidget, "btnDatos"))
-        self.button_group.addButton(botones_widget.findChild(QWidget, "btnHistorial"))
-
-        # Encuentra botones específicos
-        btn_home = botones_widget.findChild(QWidget, "btnHome")
-        btn_datos = botones_widget.findChild(QWidget, "btnDatos")
-        btn_historial = botones_widget.findChild(QWidget, "btnHistorial")
-
-        btn_home.setChecked(True)
 
         # Parte inferior: área de vistas
         self.stack = QStackedWidget()
         home_view = HomeWindow()
         datos_view = DatosView()
-        config_view = self._create_centered_page("Configuración")
+        historial_view = self._create_centered_page("Historial de datos")
 
         self.stack.addWidget(home_view)  # 0
         self.stack.addWidget(datos_view)  # 1
-        self.stack.addWidget(config_view)  # 2
+        self.stack.addWidget(historial_view)  # 2
 
         content_layout.addWidget(self.stack)
 
         # Conectar los botones para cambiar la vista
-        btn_home.clicked.connect(lambda: self.stack.setCurrentIndex(0))
-        btn_datos.clicked.connect(lambda: self.stack.setCurrentIndex(1))
-        # historial:
-        #btn_historial.clicked.connect(lambda: self.stack.setCurrentIndex(2))
+        botones_widget.btn_home.clicked.connect(lambda: self.stack.setCurrentIndex(0))
+        botones_widget.btn_datos.clicked.connect(lambda: self.stack.setCurrentIndex(1))
+        botones_widget.btn_historial.clicked.connect(lambda: self.stack.setCurrentIndex(2))
 
         # Agrega el área de contenido al layout principal
         main_layout.addWidget(content_widget)
