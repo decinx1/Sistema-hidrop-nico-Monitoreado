ef11853f665588e4fc01509c2af50db17697e1d3
vjLarios <vjosuee05@gmail.com>
2025-05-19 23:14:36 -0600

    seleccion de boton header

--- Interfaz/botonesheader.py
+++ Interfaz/botonesheader.py
@@ -21,8 +21,22 @@ class BotonesHeader(QWidget):
         self.btn_historial = self.findChild(QWidget, "btnHistorial")
 
         self.btn_home.setChecked(True)
+        self.button_group.setExclusive(True)
+        self.button_group.buttonClicked.connect(self._on_button_clicked)
 
         # Asignar iconos a los botones
         self.btn_home.setIcon(QIcon("Interfaz/icons/grafica.png"))
         self.btn_datos.setIcon(QIcon("Interfaz/icons/info.png"))
         self.btn_historial.setIcon(QIcon("Interfaz/icons/historial.png"))
+
+        # Aplica estilos para el botón seleccionado
+        # self._update_button_styles()  # Ya no se usa, todo va por QSS
+
+    def _on_button_clicked(self, button):
+        # Asegura que solo un botón esté seleccionado
+        for btn in [self.btn_home, self.btn_datos, self.btn_historial]:
+            btn.setChecked(btn is button)
+        # self._update_button_styles()  # Ya no se usa, todo va por QSS
+
+    # def _update_button_styles(self):
+    #     ...eliminado, ahora los estilos van en QSS...
